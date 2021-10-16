d9914975e711e23994de4d57d286c1a1873e92a4
João Cintra Silva <joaocintra@hotmail.com>
2021-10-16 11:15:44 +0100

    github

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #main
+import DBAccess
+import sys
+from urllib.parse import urlparse
+
+def add_tag(tag_name, url, site_name):
+    with DBAccess.driver.session() as session:
+        session.write_transaction(DBAccess.add_tag,tag_name,url, site_name)
+
+if sys.argv[1].upper() == "N":
+    print("Criar novo")
+    # print(f"Arguments count: {len(sys.argv)}")
+    i=3
+    while i<len(sys.argv):
+        print(sys.argv[i].upper())
+        url = sys.argv[2].upper()
+        add_tag(sys.argv[i].upper(), url, urlparse(url).hostname)
+        i=i+1
+
+elif sys.argv[1].upper() == "S":
+    print("Pesquisa")
+
+
+
+
+# add_tag("x", "http://google.com", "Google")
+# add_tag("y","http://google.com", "Google")
